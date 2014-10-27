f66f4b83577703a36551386fbefe87faa215ec50
Lior Asulin <Lior035@gmail.com>
2014-10-27 15:08:40 +0200

    Add files

---
+++ C14_Ex01/IReminderBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C14_Ex01
+{
+    public interface IReminderBuilder
+    {
+        void buildMessege(string i_Subject, string i_Body);
+        void buildContentsEvents(List<object> i_ListOfContent);
+        void buildUserMail(string mail);
+
+        Reminder Invite 
+        { 
+            get; 
+        }
+    }
+}
